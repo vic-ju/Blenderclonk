432f04b6a9d12d9f4dcf991ca43dee4e14433f79
Victor Jungbluth <victor.jungbluth@live.de>
2020-05-01 11:37:50 +0200

    Added resources: Actions, Icons, Textures, Clonk meshes/actions lists

---
+++ __init__.py
@@ -0,0 +1,11 @@
+bl_info = {
+    "name": "Blenderclonk",
+    "author": "Victor Jungbluth",
+    "version": (1, 0, 0),
+    "blender": (2, 82, 0),
+    "location": "View3D > Properties > BlenderKit",
+    "description": "Create and render clonks for Clonk Rage",
+    "warning": "",
+    "wiki_url": "https://github.com/vic-ju/Blenderclonk",
+    "category": "3D View",
+}
